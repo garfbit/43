86a27cd7b70244e405806546bf69c46f7dd01742
garfbit <104676660+garfbit@users.noreply.github.com>
2022-08-09 17:39:38 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 let cp = 0
-let ni = 0
-let nf = 0
+let ni = 3
+let nf = 13
 let i = ni + 1
 while (i < nf) {
     if (i % 2 == 0) {
         cp += 1
     }
     i += 1
-    basic.showNumber(cp)
 }
+basic.showNumber(cp)
